b1ee23ba8e2b9040a04d1fbecf8baf631de449a8
bomberoxenviosdosruedas <bomberox@enviosdosruedas.com>
2026-05-27 03:34:27 -0300

    chore: relax ESLint rules and disable linting during production builds

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,12 @@ const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
     "build/**",
     "next-env.d.ts",
   ],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-require-imports": "off",
+    "react-hooks/set-state-in-effect": "off",
+  },
 }];
 
 export default eslintConfig;
